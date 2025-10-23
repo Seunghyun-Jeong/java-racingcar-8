b056c71a9f9e54dd88983827be496b0aeebf6015
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-10-23 13:00:39 +0900

    feat: Car 클래스 추가 및 자동차 입력 및 시도 횟수 입력 로직 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,26 +1,85 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputCarName = Console.readLine();
+        List<Car> cars = inputCars(inputCarName);
 
-        List<String> carList = Arrays.stream(inputCarName.split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int tryCount = inputTryCount();
+    }
 
-        for (String carName : carList) {
-            if (carName.length() > 5) {
+    private static List<Car> inputCars(String carName) {
+        String[] names = carName.split(",");
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : names) {
+            name = name.trim();
+            cars.add(new Car(name));
+        }
+
+        return cars;
+    }
+
+    private static int inputTryCount() {
+        String inputTryCount = Console.readLine();
+
+        try {
+            int tryCount = Integer.parseInt(inputTryCount);
+
+            if (tryCount <= 0) {
+                throw new IllegalArgumentException("시도할 횟수는 1회 이상이어야 합니다.");
+            }
+
+            return tryCount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 숫자만 입력해 주세요.");
+        }
+
+
+    }
+
+    public static class Car {
+
+        private final String name;
+        private int position = 0;
+
+        public Car(String name) {
+            validateCarName(name);
+            this.name = name;
+        }
+
+        private void validateCarName(String name) {
+            if (name == null || name.isBlank()) {
+                throw new IllegalArgumentException("자동차 이름을 입력해 주세요.");
+            }
+
+            if (name.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
             }
         }
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int inputTryCount = Integer.parseInt(Console.readLine());
+        public void move(int randomNumber) {
+            if (randomNumber >= 4) {
+                position++;
+            }
+        }
+
+        public String getResult() {
+            return name + " : " + "-".repeat(position);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPosition() {
+            return position;
+        }
     }
 }
